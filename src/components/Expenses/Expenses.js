cae2eb0a3d7c831d6ad07e867863526bab51bae0
apkouk <cescorh@prosa.co.uk>
2021-04-24 21:42:30 +0200

    working branch

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
-function Expenses (props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses-item">
       <ExpenseItem
@@ -22,7 +22,6 @@ function Expenses (props) {
       ></ExpenseItem>
     </Card>
   );
-}
-
+};
 
 export default Expenses;
