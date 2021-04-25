129bd02e10305ea0fec427b518690eae669fc599
apkouk <cescorh@prosa.co.uk>
2021-04-25 06:08:52 +0200

    abriendo NewExpense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,7 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [showHideNewExpense, setShowHideNewExpense] = useState(false);
 
   const titleChangedHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -18,6 +19,10 @@ const ExpenseForm = (props) => {
     setEnteredDate(event.target.value);
   };
 
+  const setShowHide = () => {
+
+  }
+
   const submitHandler = (event) => {
       event.preventDefault();
 
@@ -27,8 +32,7 @@ const ExpenseForm = (props) => {
           date: new Date(enteredDate)
       }
       
-      props.onSaveExpenseData(expenseData);
-      
+      props.onSaveExpenseData(expenseData);      
       setEnteredTitle('');
       setEnteredAmount('');
       setEnteredDate('');
@@ -51,6 +55,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={setShowHide}>Cancel </button>
         <button>Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,24 +1,37 @@
-
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [showHideNewExpense, setShowHideNewExpense] = useState(false);
 
-    const saveExpenseDataHandler= (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        }
-        props.onAddExpense(expenseData);
-        console.log(expenseData);
-    }
+  const setShowHideNewExpenseHandler = () => {
+    setShowHideNewExpense(true);
+  };
 
+  const setShowHide = () => {
+      return setShowHideNewExpense(!showHideNewExpense);
+  };
 
-    return (
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-        </div>
-    )
-}
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    console.log(expenseData);
+  };
+
+  return (
+    <div className="new-expense" onLoad={setShowHideNewExpenseHandler}>
+      {!showHideNewExpense && (
+        <button onClick={setShowHide}>Add New Expense</button>
+      )}
+      {showHideNewExpense && (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      )}
+    </div>
+  );
+};
 
 export default NewExpense;
